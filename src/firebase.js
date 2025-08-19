daac2fae215d290423a68f02140c7c1179d5dc22
Daniele Maggi <danielemaggi@bluewin.ch>
2025-08-19 16:25:22 +0200

    feat: Sistema resoconti finali completo + fix Firebase config produzione

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyD97Bgyg0SwVPF9rZS4BCSWKP20LxXfaqM",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "dallelec-gestion-58a49.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "dallelec-gestion-58a49",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "dallelec-gestion-58a49.appspot.com",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "801397923579",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:801397923579:web:b07860aadfeef5c1579a5e",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-G3CLCT2ERB"
 };
 
 let firebaseApp, db, auth;
