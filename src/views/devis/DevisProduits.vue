fc01f30224ce93c74df0a0f7d41120a6fc9128d8
Dallelec Project <daniele.dallelec@gmail.com>
2026-05-21 11:50:50 +0200

    fix: filtro CDC più permissivo per accenti

--- src/views/devis/DevisProduits.vue
+++ src/views/devis/DevisProduits.vue
@@ -733,7 +733,7 @@ const devisTotal = computed(() => {
 });
 
 // Totali Chemin de Câble
-const cdcItems = computed(() => devisItems.value.filter(i => (i.nom || '').toLowerCase().includes('chemin de câble')));
+const cdcItems = computed(() => devisItems.value.filter(i => (i.nom || '').toLowerCase().includes('chemin de c')));
 const totalCDCSans = computed(() => cdcItems.value.reduce((sum, i) => sum + (Number(i.ml) || 0), 0));
 const totalCDCAvec = computed(() => cdcItems.value.reduce((sum, i) => sum + (Number(i.totalML) || 0), 0));
 
