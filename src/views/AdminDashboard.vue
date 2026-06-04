933d366bbdd8996065ed83d95c9ef25fb476755f
Dallelec Project <daniele.dallelec@gmail.com>
2026-06-04 10:39:45 +0200

    Cleanup: remove duplicate Rapports Heures button from dashboard

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -72,7 +72,6 @@
         <router-link to="/admin/gestion-heures" class="btn btn-outline-warning w-100">✏️ Gestion Heures</router-link>
       </div>
       <div class="col-md-3 m-2">
-        <router-link to="/admin/report-mensuel" class="btn btn-outline-warning w-100">Rapports Heures</router-link>
       </div>
       <div class="col-md-3 m-2">
         <router-link to="/admin/absences" class="btn btn-outline-warning w-100 position-relative">
